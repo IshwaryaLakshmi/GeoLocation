56180fe0e31ed34a0c2f1fb450e40b2568e02f4f
Lakshmi, Ishwarya <LakshmiI@advisory.com>
2016-08-20 10:12:08 +0530

    Initial Commit

---
+++ GeoLocation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GeoLocation
+//
+//  Created by Ishwarya Lakshmi on 8/20/16.
+//  Copyright (c) 2016 StarTypo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GeoLocation/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GeoLocation
+//
+//  Created by Ishwarya Lakshmi on 8/20/16.
+//  Copyright (c) 2016 StarTypo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
